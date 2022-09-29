72dcf3bf02768fea1b14b8c68560e3d6a62ff170
Joabe Nascimento <63386616+joabe-nascimento@users.noreply.github.com>
2022-09-29 16:22:23 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,38 +2,38 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
-        } else {
+        }else{
             $('.navbar').removeClass("sticky");
-        } if(this.scrollY > 500){
+        }
+        if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
-        } else{
+        }else{
             $('.scroll-up-btn').removeClass("show");
         }
     });
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+     });
+
+     var typed = new Typed(".typing", {
+         strings:["Desenvolvedor", "Designer", "Freelancer"],
+         typeSpeed:100,
+         backSpeed:60,
+         loop:true
+     });
+     var typed = new Typed(".typing-2", {
+        strings:["Desenvolvedor", "Designer", "Freelancer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
     });
 
-    var typed = new Typed(".typing" ,{
-        strings:["Desenvolvedor", "Designer", "Freelancer"],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
-    })
-
-    var typed = new Typed(".typing-2" ,{
-        strings:["Desenvolvedor", "Designer", "Freelancer"],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
-    })
-
     $('.menu-btn').click(function(){
-        $('navbar .menu').toggleClass("active");
+        $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
     $('.carousel').owlCarousel({
-        margin: 20,
+        margin:20,
         loop:true,
         autoplayTimeOut:2000,
         autoplayHoverPauser:true,
@@ -52,5 +52,4 @@ $(document).ready(function(){
             }
         }
     });
-    
 });
