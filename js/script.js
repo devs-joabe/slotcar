80e583236d85893bdbf6e7011c53d19521cc8afe
Joabe Nascimento <63386616+imagi-projects@users.noreply.github.com>
2022-09-30 23:40:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,3 +53,17 @@ $(document).ready(function(){
         }
     });
 });
+
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal())
+});
